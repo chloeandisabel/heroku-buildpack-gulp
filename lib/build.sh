74ebddb14963eae690f5645f74466a87dd18a504
charlieschwabacher <charlieschwabacher@gmail.com>
2015-02-17 10:17:56 -0600

    add read_json

--- lib/build.sh
+++ lib/build.sh
@@ -247,3 +247,14 @@ export_env_dir() {
     fi
   fi
 }
+
+# sed -l basically makes sed replace and buffer through stdin to stdout
+# so you get updates while the command runs and dont wait for the end
+# e.g. npm install | indent
+indent() {
+  c='s/^/       /'
+  case $(uname) in
+    Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
+    *)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
+  esac
+}
